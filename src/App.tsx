29d3b215112b316f3424a5403d839614dea16806
juanitoelestudiantev <juanitoelestudiantev@gmail.com>
2026-09-24 07:43:19 -0500

    eliminar componente de Login del archivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Login from './Login';
+
 import Registro from './Registro';
 
 export default function App() {
   return (
     <div>
       <Registro />
-      <Login />
     </div>
   );
 }
